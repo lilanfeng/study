485496f3985cee284e7b2ce6536accbe4b8da568
lilf <lilf@bwoil.com>
2024-03-13 18:11:33 +0800

    mq

---
+++ rabbitmq-simple/src/main/java/com/lilanfeng2089/study/Main.java
@@ -0,0 +1,13 @@
+package com.lilanfeng2089.study;
+
+/**
+ * @program: com.lilanfeng2089.study
+ * @description: ${DESCRIPTION}
+ * @author: lilf@bwoil.com
+ * @create: 2024-03-12 17:51
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
